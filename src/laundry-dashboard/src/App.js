298ff70d57a8a8240bd16157d91b0465a5972c44
Rob <roberto-jr.torres@partner.bmwgroup.com>
2019-05-27 14:46:40 -0700

    move state to App (away from LaundryMachine)

--- src/laundry-dashboard/src/App.js
+++ src/laundry-dashboard/src/App.js
@@ -1,15 +1,43 @@
 import React from 'react';
 import './App.css';
 import LaundryMachine from './LaundryMachine/LaundryMachine';
+import firebase from 'firebase';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <LaundryMachine />        
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      status: ''
+    };
+
+    this.getLaundryStatus();
+  }
+  
+  getLaundryStatus() {
+    var config = {
+      
+    };
+
+    if (!firebase.apps.length) {
+        firebase.initializeApp(config);
+    }
+    var statusListener = firebase.database().ref('/status/');
+    
+    statusListener.on('value', (status) => {
+        this.setState({
+            status: status.val()
+        });
+    });
+  }
+  
+  render() {  
+    return (
+      <div className="App">
+        <header className="App-header">
+          <LaundryMachine status={this.state.status}/>        
+        </header>
+      </div>
+    );
+  };
 }
-
 export default App;
--- src/laundry-dashboard/src/LaundryMachine/LaundryMachine.js
+++ src/laundry-dashboard/src/LaundryMachine/LaundryMachine.js
@@ -2,39 +2,13 @@ import React from 'react';
 import LaundryContainer from './LaundryContainer/LaundryContainer';
 import LaundryHeadBoard from './LaundryHeadBoard/LaundryHeadBoard';
 import './LaundryMachine.css';
-import firebase from 'firebase';
 
-class Laundry extends React.Component {
-    constructor(props) {
-        super(props);
-
-        var config = {
-          };
-
-        if (!firebase.apps.length) {
-            firebase.initializeApp(config);
-        }
-        var statusListener = firebase.database().ref('/status/');
-        
-        statusListener.on('value', (status) => {
-            this.setState({
-                status: status.val()
-            });
-        });
-        
-        this.state = { 
-            status: "",
-        };
-    }
+const LaundryMachine = (props) => (
+    <div className={"LaundryMachine " + props.status}>
+        <LaundryHeadBoard />
+        <LaundryContainer status={props.status}/>
+    </div>
+);
     
-    render() {
-        return (
-            <div className={"LaundryMachine " + this.state.status}>
-                <LaundryHeadBoard />
-                <LaundryContainer status={this.state.status}/>
-            </div>
-        );
-    }
-}
 
-export default Laundry;
+export default LaundryMachine;
