bab39bcbe25c5024e60d15ae174e0f302ccfc312
Roberto Torres <torres-roberto@outook.com>
2020-01-20 13:05:57 -0600

    refactor(motion-sensor): use local http server instead of iothub

--- src/move-detector/motion-sensor.js
+++ src/move-detector/motion-sensor.js
@@ -1,30 +1,87 @@
-var { EventHubClient, EventPosition } = require('@azure/event-hubs');
- 
-var connectionString = '';
- 
-// Connect to the partitions on the IoT Hub's Event Hubs-compatible endpoint.
-// This example only reads messages sent after this application started.
-var ehClient;
+const firebase = require('firebase');
+const express = require('express')                                                     
+const dotenv = require('dotenv');
 
-var printMessage = function (message) {
-    console.log('Telemetry received: ');
-    //console.log(JSON.stringify(message.body));
-    console.log('Movement: ' + message.body.data);
-    console.log('');
+dotenv.config();
+
+var statusListener;
+
+const port = process.env.PORT;                                                                      
+
+let laundryStoreStatus = 'isDone';
+
+function getLaundryStatus() {
+  var config = {
+    apiKey: `${process.env.FIREBASE_APIKEY}`,
+    databaseURL: `${process.env.FIREBASE_DBURL}`,
+    storageBucket: `${process.env.FIREBASE_STORAGEBUCKET}`
   };
-
-var printError = function (err) {
-    console.log(err.message);
-  };
-
-EventHubClient.createFromIotHubConnectionString(connectionString).then(function (client) {
-  console.log("Successully created the EventHub Client from iothub connection string.");
-  ehClient = client;
-  return ehClient.getPartitionIds();
-}).then(function (ids) {
-  console.log("The partition ids are: ", ids);
-  return ids.map(function (id) {
-    return ehClient.receive(id, printMessage, printError, { eventPosition: EventPosition.fromEnqueuedTime(Date.now()) });
-  });
-}).catch(printError);
   
+  if (!firebase.apps.length) {
+    firebase.initializeApp(config);
+  }
+  statusListener = firebase.database().ref('/status/');
+  
+  statusListener.on('value', (status) => {
+    laundryStoreStatus = status.val();
+  });
+}
+
+
+let position = {
+  x: '',
+  y: '',
+  z: ''
+};
+
+function updateLaundryStatus(status) {
+  console.log(laundryStoreStatus, '=>', status, position);
+  if (status === 'isDone' && laundryStoreStatus !== 'isDone') {
+    position = {
+      x: '',
+      y: '',
+      z: ''
+    };
+
+    laundryStoreStatus = 'isDone';
+    if (!firebase.apps.length) {
+      firebase.initializeApp(config);
+    }
+    statusListener.set(status)
+  }
+
+  if (status === 'isWashing' && laundryStoreStatus !== 'isWashing') {
+    laundryStoreStatus = 'isWashing';
+    statusListener.set(status)
+  }
+}
+
+function checkStatus(data) {
+  const [x, y, z] = [data.x, data.y, data.z];
+
+  if (position.x === x ||
+      position.y === y ||
+      position.z === z) {
+        updateLaundryStatus('isDone');
+  } else {
+        position.x = x;
+        position.y = y;
+        position.z = z;
+        updateLaundryStatus('isWashing');
+  }
+}
+
+const app = express()                                                                  
+
+let count = 0;                                                                         
+
+getLaundryStatus();
+
+app.get('/x/:x/y/:y/z/:z', (req, res) =>  {
+  console.log(++count, 'movement: ', req.params);
+  checkStatus(req.params);
+});
+  
+app.listen(port, '0.0.0.0', function() {
+  console.log('Listening to port:  ' + port);
+}); 
